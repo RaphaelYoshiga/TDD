1d86fd3b610c384e04bb48977313131024d20618
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2019-01-15 23:30:30 +0000

    String calculator kata

---
+++ Outside.Tdd.1FizzBuzz/StringCalculatorShould.cs
@@ -0,0 +1,58 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Outside.Tdd._1FizzBuzz
+{
+    public class StringCalculatorShould
+    {
+        private readonly StringCalculator _stringCalculator;
+
+        public StringCalculatorShould()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [Fact]
+        public void DefaultToZeroOnEmptyString()
+        {
+            var result = _stringCalculator.Calculate("");
+            result.ShouldBe(0);
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("155", 155)]
+        [InlineData("9", 9)]
+        public void ReturnNumberIfOneArgument(string input, int expected)
+        {
+            var result = _stringCalculator.Calculate(input);
+            result.ShouldBe(expected);
+        }
+
+        [Theory]
+        [InlineData("1,1", 2)]
+        [InlineData("10,1", 11)]
+        [InlineData("99,11", 110)]
+        public void SumNumbersSeparatedByComma(string input, int expected)
+        {
+            var result = _stringCalculator.Calculate(input);
+            result.ShouldBe(expected);
+        }
+    }
+
+
+    public class StringCalculator
+    {
+        public int Calculate(string input)
+        {
+            if (input == string.Empty)
+                return 0;
+
+            var splittedInput = input.Split(';');
+            if (splittedInput.Length == 1)
+                return int.Parse(input);
+
+            return int.Parse(splittedInput[0]) + int.Parse(splittedInput[1]);
+        }
+    }
+}
